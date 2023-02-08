c205025b6fa30bcfdd7f65652f6505745415ef79
ahmadbahrun <redgaming18@gmail.com>
2023-02-08 21:50:19 +0700

    Pindah logic activity

--- app/src/main/java/com/example/chapterchallenge7/signup/SignUpScreenActivity.kt
+++ app/src/main/java/com/example/chapterchallenge7/signup/SignUpScreenActivity.kt
@@ -7,26 +7,5 @@ import com.example.chapterchallenge7.R
 import com.example.chapterchallenge7.databinding.ActivitySignUpScreenBinding
 
 class SignUpScreenActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySignUpScreenBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        enableButton()
-
-        binding.etLoginEmail.doAfterTextChanged {
-            enableButton()
-        }
-        binding.etLoginPassword.doAfterTextChanged {
-            enableButton()
-        }
-    }
-
-    private fun enableButton() {
-        val email = binding.etLoginEmail.text.toString()
-        val password = binding.etLoginPassword.text.toString()
-
-        binding.btnLogin.isEnabled = email.isNotEmpty() && password.isNotEmpty()
-    }
 }
