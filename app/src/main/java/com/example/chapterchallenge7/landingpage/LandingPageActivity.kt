cf09acb38b83c2334c400b8219d9da052f2c2b1e
Brian Ajusta <brian.ajusta04@gmail.com>
2023-02-08 23:06:56 +0700

    cek

--- app/src/main/java/com/example/chapterchallenge7/landingpage/LandingPageActivity.kt
+++ app/src/main/java/com/example/chapterchallenge7/landingpage/LandingPageActivity.kt
@@ -3,8 +3,12 @@ package com.example.chapterchallenge7.landingpage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chapterchallenge7.R
+import com.example.chapterchallenge7.databinding.ActivityLandingPageBinding
 
 class LandingPageActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityLandingPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
