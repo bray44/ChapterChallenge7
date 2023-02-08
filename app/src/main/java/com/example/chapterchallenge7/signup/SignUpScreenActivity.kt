fea1a828a29af7b395871828c6bca9e5332c666c
ahmadbahrun <redgaming18@gmail.com>
2023-02-08 21:23:03 +0700

    add splash screen add landing page and add login

--- app/src/main/java/com/example/chapterchallenge7/signup/SignUpScreenActivity.kt
+++ app/src/main/java/com/example/chapterchallenge7/signup/SignUpScreenActivity.kt
@@ -2,11 +2,31 @@ package com.example.chapterchallenge7.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
 import com.example.chapterchallenge7.R
+import com.example.chapterchallenge7.databinding.ActivitySignUpScreenBinding
 
 class SignUpScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_screen)
+        binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableButton()
+
+        binding.etLoginEmail.doAfterTextChanged {
+            enableButton()
+        }
+        binding.etLoginPassword.doAfterTextChanged {
+            enableButton()
+        }
+    }
+
+    private fun enableButton() {
+        val email = binding.etLoginEmail.text.toString()
+        val password = binding.etLoginPassword.text.toString()
+
+        binding.btnLogin.isEnabled = email.isNotEmpty() && password.isNotEmpty()
     }
 }
