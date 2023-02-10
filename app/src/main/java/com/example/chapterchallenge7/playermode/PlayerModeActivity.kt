942417052ec5bfabc0988db757cb1ab07430c022
Fahmi <fahmijamaludin2@gmail.com>
2023-02-11 00:31:47 +0700

    add logic

--- app/src/main/java/com/example/chapterchallenge7/playermode/PlayerModeActivity.kt
+++ app/src/main/java/com/example/chapterchallenge7/playermode/PlayerModeActivity.kt
@@ -1,12 +1,34 @@
 package com.example.chapterchallenge7.playermode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chapterchallenge7.R
+import com.example.chapterchallenge7.databinding.ActivityPlayerModeBinding
+import com.example.chapterchallenge7.gameplay.GameplayVsComActivity
+import com.example.chapterchallenge7.gameplay.GameplayVsPlayerActivity
+import com.example.chapterchallenge7.mainmenu.MainMenuActivity
 
 class PlayerModeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlayerModeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player_mode)
+        binding = ActivityPlayerModeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        supportActionBar?.hide()
+
+        binding.ivClose.setOnClickListener {
+            startActivity(Intent(this, MainMenuActivity::class.java))
+        }
+
+        binding.player.setOnClickListener {
+            startActivity(Intent(this, GameplayVsPlayerActivity::class.java))
+        }
+
+        binding.computer.setOnClickListener {
+            startActivity(Intent(this, GameplayVsComActivity::class.java))
+        }
     }
 }
