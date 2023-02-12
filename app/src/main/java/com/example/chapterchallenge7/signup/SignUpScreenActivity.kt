4f6f7442898f267d96acb39d1c0d28c59e60ce84
rabbil <rabbil.nurrahmat@gmail.com>
2023-02-12 22:59:10 +0700

    update logic signup

--- app/src/main/java/com/example/chapterchallenge7/signup/SignUpScreenActivity.kt
+++ app/src/main/java/com/example/chapterchallenge7/signup/SignUpScreenActivity.kt
@@ -1,11 +1,67 @@
 package com.example.chapterchallenge7.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import com.example.chapterchallenge7.R
 import com.example.chapterchallenge7.databinding.ActivitySignUpScreenBinding
+import com.example.chapterchallenge7.login.LoginActivity
+import com.example.chapterchallenge7.mvvm.data.api.RetrofitBuilder
+import com.example.chapterchallenge7.mvvm.data.repository.Repository
+import com.example.chapterchallenge7.mvvm.ui.register.RegisterViewModel
+import com.example.chapterchallenge7.mvvm.utils.Status
+import com.google.android.material.snackbar.Snackbar
 
 class SignUpScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpScreenBinding
+    private lateinit var viewModel: RegisterViewModel
+    private val sharedPreferences by lazy {
+        applicationContext.getSharedPreferences("AUTHENTICATION", MODE_PRIVATE)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
+        viewModel = RegisterViewModel(
+            Repository(
+                RetrofitBuilder.apiService(sharedPreferences),
+                sharedPreferences
+            )
+        )
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener {
+            viewModel.register(
+                password = binding.etSignupPassword.text.toString(),
+                username = binding.etSignupName.text.toString(),
+            ).observe(this) {
+                when (it.status) {
+                    Status.SUCCESS -> {
+                        binding.btnSignup.visibility = View.VISIBLE
+                        binding.pbLoading.visibility = View.GONE
+                        Snackbar.make(
+                            binding.root, it.data?.data?.id.orEmpty(), Snackbar.LENGTH_INDEFINITE
+                        ).show()
+                    }
+                    Status.LOADING -> {
+                        binding.btnSignup.visibility = View.GONE
+                        binding.pbLoading.visibility = View.VISIBLE
+                    }
+                    Status.ERROR -> {
+                        binding.btnSignup.visibility = View.VISIBLE
+                        binding.pbLoading.visibility = View.GONE
+                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
+        binding.tvSignUpTitle.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
 
 }
